f8a71a3cd4673204ff994a056f4c53485be4f4f5
deninurlandi <badjolex14@gmail.com>
2025-10-27 21:45:39 +0700

    menambahkan progres pembuatan projek

--- script.js
+++ script.js
@@ -67,3 +67,46 @@ function salinRekening() {
       setTimeout(() => (btn.innerHTML = 'Salin'), 1500);
     });
 }
+function salinRekening2() {
+  const text = document.getElementById('rekening2').innerText.trim();
+  const btn = document.getElementById('btnSalin2');
+
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      const awal = btn.innerHTML;
+      // ✅ Ganti jadi ikon centang + teks
+      btn.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-check2-square" viewBox="0 0 16 16">
+  <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z"/>
+  <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0"/>
+</svg>
+        Copy
+      `;
+      btn.disabled = true;
+      setTimeout(() => {
+        btn.innerHTML = awal; // balikin lagi ke tampilan awal
+        btn.disabled = false;
+      }, 1500);
+    })
+    .catch(() => {
+      // fallback kalau clipboard gagal
+      const ta = document.createElement('textarea');
+      ta.value = text;
+      document.body.appendChild(ta);
+      ta.select();
+      document.execCommand('copy');
+      document.body.removeChild(ta);
+
+      btn.innerHTML = `✅ Disalin`;
+      setTimeout(() => (btn.innerHTML = 'Salin'), 1500);
+    });
+}
+
+const btnGift = document.querySelector('.btn-gift-card');
+const giftBack = document.querySelector('.gift-back');
+
+btnGift.addEventListener('click', () => {
+  giftBack.classList.toggle('show');
+  btnGift.classList.toggle('active');
+});
